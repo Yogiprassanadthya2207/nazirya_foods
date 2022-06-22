b8ea754ef605fc0341d3380c4015e27b9dcf0f95
Yogiprassanadthya2207 <91770871+Yogiprassanadthya2207@users.noreply.github.com>
2022-06-22 21:08:33 +0530

    april_2nd_upload

---
+++ touch.sh
@@ -0,0 +1,6 @@
+git init
+git remote add origin https://github.com/Yogiprassanadthya2207/nazirya_foods.git
+git remote -v
+git add . --force
+git commit -m "april_2nd_upload" 
+git push origin main --force
